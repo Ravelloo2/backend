f886dc2df34ff11c59c85b265869bcdfd39eefec
Eliaz Ravello <eliaz@telia.com>
2022-01-14 11:53:44 +0000

    Auth fail version

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const res = require('express/lib/response')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.status(200).send({"STATUS":"SUCCESS"})
+    res.set('WWW-Authenicate', 'Basic realm="Access to Index"')
+    res.status(401).send('Unauthorized Access')
 })
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
-})
+});
